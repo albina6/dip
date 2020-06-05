723708c54575067171a23c67f22cdbbed707b0cb
albina6 <49582478+albina6@users.noreply.github.com>
2020-06-05 21:55:14 +0500

    first, only forms

---
+++ genAlg1/genAlg1/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace genAlg1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label deliveryL;
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.issueTB = new System.Windows.Forms.TextBox();
+            this.issueL = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calculateB = new System.Windows.Forms.Button();
+            this.matrixB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            deliveryL = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deliveryL
+            // 
+            deliveryL.AutoSize = true;
+            deliveryL.Location = new System.Drawing.Point(16, 85);
+            deliveryL.Name = "deliveryL";
+            deliveryL.Size = new System.Drawing.Size(94, 13);
+            deliveryL.TabIndex = 3;
+            deliveryL.Text = "Адреса доставки";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новыйToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // новыйToolStripMenuItem
+            // 
+            this.новыйToolStripMenuItem.Name = "новыйToolStripMenuItem";
+            this.новыйToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.новыйToolStripMenuItem.Text = "Новый";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // issueTB
+            // 
+            this.issueTB.Location = new System.Drawing.Point(16, 62);
+            this.issueTB.Name = "issueTB";
+            this.issueTB.Size = new System.Drawing.Size(171, 20);
+            this.issueTB.TabIndex = 1;
+            // 
+            // issueL
+            // 
+            this.issueL.AutoSize = true;
+            this.issueL.Location = new System.Drawing.Point(16, 46);
+            this.issueL.Name = "issueL";
+            this.issueL.Size = new System.Drawing.Size(115, 13);
+            this.issueL.TabIndex = 2;
+            this.issueL.Text = "Адрес пункта выдачи";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 102);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 135);
+            this.textBox1.TabIndex = 4;
+            // 
+            // calculateB
+            // 
+            this.calculateB.Location = new System.Drawing.Point(16, 273);
+            this.calculateB.Name = "calculateB";
+            this.calculateB.Size = new System.Drawing.Size(165, 23);
+            this.calculateB.TabIndex = 5;
+            this.calculateB.Text = "Построение маршрута";
+            this.calculateB.UseVisualStyleBackColor = true;
+            // 
+            // matrixB
+            // 
+            this.matrixB.Location = new System.Drawing.Point(16, 244);
+            this.matrixB.Name = "matrixB";
+            this.matrixB.Size = new System.Drawing.Size(171, 23);
+            this.matrixB.TabIndex = 6;
+            this.matrixB.Text = "Матрица";
+            this.matrixB.UseVisualStyleBackColor = true;
+            this.matrixB.Click += new System.EventHandler(this.matrixB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Количество пунктов";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(16, 319);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.matrixB);
+            this.Controls.Add(this.calculateB);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(deliveryL);
+            this.Controls.Add(this.issueL);
+            this.Controls.Add(this.issueTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Расчет маршрута близкого к оптимальному";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.TextBox issueTB;
+        private System.Windows.Forms.Label issueL;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button calculateB;
+        private System.Windows.Forms.Button matrixB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
